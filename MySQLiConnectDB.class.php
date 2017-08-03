cc9f09e642a04c7c45f80052defab73e03509024
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-08-03 19:10:21 +0800

    Update MySQLiConnectDB.class.php

--- MySQLiConnectDB.class.php
+++ MySQLiConnectDB.class.php
@@ -4,8 +4,10 @@ class ConnectDB {
     // initialize connection
     function __construct() { 
         // 先寫死連線帳密，之後再看看要怎麼改
-        $this->connect = mysqli_connect("host","account","password","database");
-        If(!$this->connect) throw new Exception("Filed to connect DB.");
+        $this->connect = mysqli_connect("不告訴你","不告訴你","不告訴你","不告訴你");
+        If(!$this->connect) {
+            throw new Exception("Filed to connect DB.");
+        }
         // Set connection encoding
         mysqli_query($this->connect,"SET NAMES 'utf8'");
     }
@@ -18,6 +20,25 @@ class ConnectDB {
     public function query($str) { 
         return mysqli_query($this->connect, $str);
         // not catch exception here
+        // sql injection warning
+    }
+    public function executePreparedStatement($prepared, $paramString = ''){
+        $stmt = $this->connect->prepare($prepared);
+        if (func_num_args() > 2){
+            $parameters = func_get_args();
+            array_shift($parameters); // remove the prepared from the list
+            // Array needs to be bound by reference
+            foreach ($parameters as $key=>&$value) {
+                $parameters[$key] = &$value;
+            }
+            call_user_func_array(array($stmt, "bind_param"), $parameters);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result;
     }
 };
+// 2017-07-31 18:59 --
+//   PHP5 -> PHP7 的部分差不多完成了
 ?>
